35022f13adb9b899d5b62b0d6e795ac943050cf5
Tom Arn <sw@t-arn.com>
2023-02-16 18:38:36 +0100

    7

--- src/spectre_algorithm.py
+++ src/spectre_algorithm.py
@@ -27,104 +27,118 @@ import hmac
 import hashlib
 from spectre_types import spectreTypes
 
+
 class SpectreError(Exception):
-    
+
     def __init__(self, cause, message):
         self.cause = cause
         self.message = message
         super().__init__(self.message)
     # __init__
 
+
 # SpectreError
 
 
 def uint32_to_bytes(x: int) -> bytes:
     return x.to_bytes(4, byteorder='big', signed=False)
+
+
 # int_to_bytes
 
 
 class Spectre:
-    
+
     def newUserKey(self, userName, userSecret, algorithmVersion=spectreTypes.algorithm["current"]):
         print(f"[spectre]: userKey={userName}, algorithmVersion={algorithmVersion}\n")
 
-        if (algorithmVersion < spectreTypes.algorithm["first"] || algorithmVersion > spectreTypes.algorithm["last"]):
+        if algorithmVersion < spectreTypes.algorithm["first"] or algorithmVersion > spectreTypes.algorithm["last"]:
             raise SpectreError("algorithmVersion", f"Unsupported algorithm version: {algorithmVersion}.")
-        else if (userName is None || len(userName)==0):
+        elif userName is None or len(userName) == 0:
             raise SpectreError("userName", "Missing user name.")
-        else if (userSecret is None || len(userSecret)==0):
+        elif userSecret is None or len(userSecret) == 0:
             raise SpectreError("userSecret", "Missing user secret.")
-    
+
         try:
             userSecretBytes = bytes(userSecret)
             userNameBytes = bytes(userName)
-            keyPurpose = bytes(spectre.purpose["authentication"])
-    
+            keyPurpose = bytes(spectreTypes.purpose["authentication"])
+
             # 1. Populate user salt: scope | #userName | userName
             userSalt = keyPurpose
-    
-            if (algorithmVersion < 3):
+
+            if algorithmVersion < 3:
                 # V0, V1, V2 incorrectly used the character length instead of the byte length.
                 userSalt += uint32_to_bytes(len(userName))
             else:
                 userSalt += uint32_to_bytes(len(userNameBytes))
-    
+
             userSalt += userNameBytes
-    
+
             # 2. Derive user key from user secret and user salt.
-            userKeyData = hashlib.scrypt(userSecretBytes, userSalt, 32768, 8, 2, 64)
+            userKeyData = hashlib.scrypt(userSecretBytes, salt=userSalt, n=32768, r=8, p=2, dklen=64)
             userKeyCrypto = hmac.new(userKeyData, msg=None, digestmod=hashlib.sha256).digest()
             return {"keyCrypto": userKeyCrypto, "keyAlgorithm": algorithmVersion}
         except Exception as ex:
             raise ex
     # newUserKey
-    
-    
+
+
 # Spectre
 
 spectre = Spectre()
 
-    
+
 class SpectreUser:
-    
-    def __init__(self, userName, userSecret, algorithmVersion = spectreTypes.algorithm["current"]):
+
+    def __init__(self, userName, userSecret, algorithmVersion=spectreTypes.algorithm["current"]):
         self.userName = userName
         self.algorithmVersion = algorithmVersion
-        self.identiconPromise = spectre.newIdenticon(userName, userSecret)
+        # todo:
+        # self.identiconPromise = spectre.newIdenticon(userName, userSecret)
         self.userKeyPromise = spectre.newUserKey(userName, userSecret, algorithmVersion)
+
     # __init__
 
-    def password(self, siteName, resultType = spectre.resultType["defaultPassword"],
-                       keyCounter = spectre.counter["default"], keyContext = None):
-        userKey = self.userKeyPromise
-        return self.result(userKey, siteName, resultType, keyCounter, spectre.purpose["authentication"], keyContext)
+    def password(self, siteName, resultType=spectreTypes.resultType["defaultPassword"],
+                 keyCounter=spectreTypes.counter["default"], keyContext=None):
+        # not used?
+        # userKey = self.userKeyPromise
+        return self.result(siteName, resultType, keyCounter, spectreTypes.purpose["authentication"], keyContext)
+
     # password
 
-    def login(self, siteName, resultType = spectre.resultType["defaultLogin"],
-                keyCounter = spectre.counter["default"], keyContext = None):
+    def login(self, siteName, resultType=spectreTypes.resultType["defaultLogin"],
+              keyCounter=spectreTypes.counter["default"], keyContext=None):
         userKey = self.userKeyPromise
-        return self.result(userKey, siteName, resultType, keyCounter, spectre.purpose["identification"], keyContext)
+        return self.result(userKey, siteName, resultType, keyCounter, spectreTypes.purpose["identification"],
+                           keyContext)
+
     # login
 
-    def answer(self, siteName, resultType = spectre.resultType["defaultAnswer"],
-                 keyCounter = spectre.counter["default"], keyContext = None):
+    def answer(self, siteName, resultType=spectreTypes.resultType["defaultAnswer"],
+               keyCounter=spectreTypes.counter["default"], keyContext=None):
         userKey = self.userKeyPromise
-        return self.result(userKey, siteName, resultType, keyCounter, spectre.purpose["recovery"], keyContext)
+        return self.result(userKey, siteName, resultType, keyCounter, spectreTypes.purpose["recovery"], keyContext)
+
     # answer
 
     def result(self, siteName, resultType, keyCounter, keyPurpose, keyContext):
-        userKey = this.userKeyPromise
+        userKey = self.userKeyPromise
         return spectre.newSiteResult(userKey, siteName, resultType, keyCounter, keyPurpose, keyContext)
+
     # result
 
     def invalidate(self):
         self.userKeyPromise = SpectreError("invalidate", "User logged out.")
+
     # invalidate
 
+    @staticmethod
     def test():
         user = SpectreUser("Robert Lee Mitchell", "banana colored duckling")
         password = user.authenticate("masterpasswordapp.com")
-        if (password != "Jejr5[RepuSosp"):
+        if password != "Jejr5[RepuSosp":
             raise Exception("Internal consistency test failed.")
     # test
 # SpectreUser
